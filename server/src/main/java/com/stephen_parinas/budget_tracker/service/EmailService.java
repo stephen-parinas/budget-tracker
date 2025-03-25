22dd7ea57c8beaa89fd27a8da1c936947f370056
Stephen Pariñas <npar276@aucklanduni.ac.nz>
2025-03-26 00:16:44 +1300

    email configuration and service

---
+++ server/src/main/java/com/stephen_parinas/budget_tracker/service/EmailService.java
@@ -0,0 +1,34 @@
+package com.stephen_parinas.budget_tracker.service;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+/**
+ * A service class for handling email sending operations.
+ */
+@Service
+public class EmailService {
+    @Autowired
+    private JavaMailSender mailSender;
+
+    /**
+     * Sends a verification email to the specified recipient.
+     *
+     * @param to      The recipient's email address.
+     * @param subject The subject of the email.
+     * @param body    The HTML body content of the email.
+     * @throws MessagingException If there is an issue creating or sending the email.
+     */
+    public void sendVerificationEmail(String to, String subject, String body) throws MessagingException {
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+
+        helper.setTo(to);
+        helper.setSubject(subject);
+        helper.setText(body, true);
+    }
+}
